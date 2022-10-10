79cdbef6f1b047b8ee2c658344a2692600fd9605
halca322 <halca322@student.otago.ac.nz>
2022-10-10 23:40:17 +1300

    Strings 1

--- first.cpp
+++ first.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
+#include <string>
+
 using std::cout;
 using std::cin;
-
+using std::string;
 
 
 void print_power(int x, int y);
 
 
-int main(){
-    int x, y;
-    cout << "Base: ";
-    cin >> x;
-    cout << "Exp: ";
-    cin >> y;
-    print_power(x, y);
+int main()
+{
+    // int x, y;
+    // cout << "Base: ";
+    // cin >> x;
+    // cout << "Exp: ";
+    // cin >> y;
+    // print_power(x, y);
+    string greeting = "Hello";
+    greeting.insert(3, "");
+    greeting.erase(4);
+    cout << greeting << std::endl;
+
+    string test;
+    cout << "Test multi: ";
+    getline(cin, test);
+    cout << "test: " + test << std::endl;
+    
 }
 
-void print_power(int x, int y) {
+void print_power(int x, int y) 
+{
     int base = 1;
     for(int i = 0; i < y; i++) {
         base = base * x;
