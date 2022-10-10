b6d7ed8120bc6eeb7970fe34fb9f581f626dfe7b
halca322 <halca322@student.otago.ac.nz>
2022-10-11 12:09:55 +1300

    game 1

--- game.cpp
+++ game.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+
+using std::cout, std::cin, std::endl, std::string;
+
 
 int main()
 {
+    int choice;
+    do
+    {
+        cout << "0. Quit" << endl << "1. Play" << endl;
+        cin >> choice;
+
+        switch(choice)
+        {
+        case 0:
+            cout << "Bye" << endl;
+            return 0;
+        case 1:
+            cout << "Lets gooo" << endl;
+            break;
+        }
+    } while (choice != 0);
+    
     
 }
