7b58d7185398ae1ded66ad4bdaa6d69aedf81d94
halca322 <halca322@student.otago.ac.nz>
2022-10-11 13:22:19 +1300

    game refactored to use vector

--- game.cpp
+++ game.cpp
@@ -2,18 +2,34 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
-using std::cout, std::cin, std::endl, std::string;
+using std::cout, std::cin, std::endl, std::string, std::vector;
 
+
+void print_vector(vector<int> vec)
+{
+    for(int i = 0; i < vec.size(); i++)
+    {
+        cout << vec[i] << '\t';
+    }
+    cout << "\n";
+}
 
 void play_game(){
+    vector<int> guesses;
+    int guess_count;
     int random = rand() % 251; //rand needs a pointer for max
     cout << random << endl;
+
     while(true){
         int guess;
         cin >> guess;
+        guesses.push_back(guess);
+
         if(guess == random) {
             cout << "You win!\n\n";
+            print_vector(guesses);
             break;
         } else if (guess < random) {
             cout << "Too low\n";
@@ -22,6 +38,7 @@ void play_game(){
         }
     }
 }
+
 
 int main()
 {
