6c43c2f5511fbcc111a5187b76d5c9d906e7b1e1
halca322 <halca322@student.otago.ac.nz>
2022-10-11 16:31:09 +1300

    default args better than overloading

--- fifth.cpp
+++ fifth.cpp
@@ -12,12 +12,12 @@ struct Rectangle
     double width;
 };
 
-double area(double length, double width)
+double area(double length, double width) //rectangle
 {
     return length * width;
 }
  //overload 1
-double area(double length)
+double area(double length) //square
 {
     return length * length;
 }
@@ -28,11 +28,25 @@ double area(Rectangle rectangle)
     return rectangle.length * rectangle.width;
 }
 
+// default arguments! OP
+double pow(double base, int pow = 2)
+{
+    int total = 1;
+    for(int i = 0; i<pow; i++)
+    {
+        total *= base;
+    }
+    return total;
+}
+
 int main()
 {
     Rectangle rectangle;
     rectangle.length = 10;
     rectangle.width = 10;
-    cout << area(rectangle.length, rectangle.width);
+    cout << area(rectangle.length, rectangle.width) << endl;
     
+    cout << pow(3,3) << endl;
+    cout << pow(3) << endl;
+
 }
