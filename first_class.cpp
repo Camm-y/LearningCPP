083dcf51e253e4e307fdab37968d65b085695bcf
halca322 <halca322@student.otago.ac.nz>
2022-10-11 18:02:53 +1300

    classes

---
+++ first_class.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class User
+{
+    std::string status = "Gold";
+
+    public:
+        std::string first_name;
+        std::string last_name;
+        std::string get_status()
+        {
+            return status;
+        }
+};
+
+int main()
+{
+    User me;
+    me.first_name = "Cam";
+    me.last_name = "Hall";
+
+    std::cout << "Status: " << me.get_status() << std::endl;
+}
