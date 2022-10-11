596b08a6126e4b7297b9bea8ece1c585477a6947
halca322 <halca322@student.otago.ac.nz>
2022-10-11 23:59:26 +1300

    class encapsulation

--- first_class.cpp
+++ first_class.cpp
@@ -4,13 +4,30 @@
 class User
 {
     std::string status = "Gold";
-
+    std::string first_name;
+    std::string last_name;
     public:
-        std::string first_name;
-        std::string last_name;
+        
         std::string get_status()
         {
             return status;
+        }
+        void set_status(std::string status)
+        {
+            this->status = status;
+        }
+
+        std::string get_fname(){
+            return first_name;
+        }
+        void set_fname(std::string fname){
+            this->first_name = fname;
+        }
+        std::string get_lname(){
+            return last_name;
+        }
+        void set_lname(std::string lname){
+            this->last_name = lname;
         }
 
     User()
@@ -29,31 +46,31 @@ class User
     }
 };
 
-int add_ifndef(std::vector<User> &users, User user)
-{
-    for(int i=0; i<users.size(); i++)
-    {
-        if(users[i].first_name == user.first_name &&
-        users[i].last_name == user.last_name)
-        {
-            return i;
-        }
-    }
-    users.push_back(user);
-    return users.size() - 1;
-}
+// int add_ifndef(std::vector<User> &users, User user)
+// {
+//     for(int i=0; i<users.size(); i++)
+//     {
+//         if(users[i].first_name == user.first_name &&
+//         users[i].last_name == user.last_name)
+//         {
+//             return i;
+//         }
+//     }
+//     users.push_back(user);
+//     return users.size() - 1;
+// }
 
 void user_testing1()
 {
     User me;
-    me.first_name = "Cam";
-    me.last_name = "Hall";
+    me.set_fname("Cam");
+    me.set_lname("Hall");
 
     std::cout << "Status: " << me.get_status() << std::endl;
 
     std::vector<User> users;
     users.push_back(me);
-    std::cout << users[0].first_name << std::endl;
+    std::cout << users[0].get_fname() << std::endl;
 }
 
 int main()
