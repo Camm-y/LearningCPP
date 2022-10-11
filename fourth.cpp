c17faffa23d476b819642c78b4f96466119bbbdf
halca322 <halca322@student.otago.ac.nz>
2022-10-11 16:10:49 +1300

    multidimensional arrays and vectors

--- fourth.cpp
+++ fourth.cpp
@@ -5,9 +5,22 @@
 using namespace std;
 
 
+void multi_arrays()
+{
+    vector<vector<int>> grades = {{1, 2, 3},{3, 4, 3},{5, 6, 3}};
+    //int grades [][3] = {{1, 2, 3},{3, 4, 3},{5, 6, 3}};
 
+    for(int r = 0; r < grades.size(); r++)
+    {
+        for(int c = 0; c < grades[r].size(); c++)
+        {
+            cout << grades[r][c] << "\t";
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
-
+    
 }
