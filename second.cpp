59e92caba72c3833b016ff4ab9178f67b8fcfef4
halca322 <halca322@student.otago.ac.nz>
2022-10-11 13:45:13 +1300

    foreach

--- second.cpp
+++ second.cpp
@@ -18,11 +18,15 @@ void change_vector(vector<int> &data)
     data.push_back(12); //saves because passing value
 }
 
+void iterating(){
+    int data[] = {1,2,3,4,5};
+    for(int n : data)
+    {
+        cout << n << "\t";
+    }
+}
 
 int main() 
 {
-    vector<int> data = {1,2,3};
-    print_vector(data);
-    change_vector(data);
-    print_vector(data);
+    
 }
